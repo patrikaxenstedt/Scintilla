ac1fa9c19b5723ca23694f10ec8c0ee789e99626
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-12 16:00:57 +0200

    added basic create-react-app

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const app = express();
-
-require('./models/User');
-require('dotenv').config()
-require('./config/passport')(passport);
-
 // Bodyparser middleware
-app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
+
+// Passport middleware
 app.use(passport.initialize());
-app.use(passport.session());
-
-// Use routes
-app.use('/api/auth', auth);
-app.use('/api/users', users);
-
+// Passport config
+require("./config/passport")(passport);
 // Routes
-const auth = require('./routes/api/auth');
-const users = require('./routes/api/users');
+app.use("/api/users", users);
+
+
+require('dotenv').config()
 
 // Connect to MongoDB
 mongoose.connect(
