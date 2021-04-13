5424fbb25930ba31d099f8744132b0ad8fbe330c
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-13 18:58:49 +0200

    added landingpage

---
+++ frontend/src/components/layout/Landing.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Startpage</h1>
+          <p>Lorem Ipsum text</p>
+          {/*  <div>
+            <Link to="/register">Register</Link>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
