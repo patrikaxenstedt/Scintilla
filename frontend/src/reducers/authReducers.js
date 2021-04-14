e9ba9b2dd19ecefbecb56c1fe540001ebd2e2eae
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-14 08:49:37 +0200

    fixed anonymous default export varning

--- frontend/src/reducers/authReducers.js
+++ frontend/src/reducers/authReducers.js
@@ -5,7 +5,7 @@ const initialState = {
   user: {},
 };
 
-export default function (state = initialState, action) {
+export default function foo(state = initialState, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
