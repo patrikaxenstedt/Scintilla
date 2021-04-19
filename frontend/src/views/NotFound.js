819489e120112dad3561e1aeeb31c563349634e9
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-19 15:29:05 +0200

    added not found page

--- frontend/src/views/NotFound.js
+++ frontend/src/views/NotFound.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
     <Hero>
       <Banner title="404" subtitle="Wups! Could not find the page..">
-        <Link to="/" className="btn-primary">
+        <Link to="/" className="nav-icon">
           return home
         </Link>
       </Banner>
