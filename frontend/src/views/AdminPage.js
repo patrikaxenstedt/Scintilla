840cc1ed93abd7d6309a1f5a67ba3a4ed55d785d
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-22 09:44:58 +0200

    created basic adminpage

---
+++ frontend/src/views/AdminPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+class AdminPage extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Footer />
+      </>
+    );
+  }
+}
