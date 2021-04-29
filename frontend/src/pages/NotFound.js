4a9be6688259595349aee39f8e4761a93d81611e
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-29 14:13:31 +0200

    404page styling now done 💅

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Wave from '../components/UI/Wave';
 import Lottie from 'lottie-react';
 import Screen from '../components/UI/Screen';
-
+import Hero from '../components/UI/Hero';
+import Welcome from '../components/Home/Welcome';
 import heroAnimation from '../assets/lottie/404.json';
 
 class NotFound extends Component {
@@ -13,10 +14,19 @@ class NotFound extends Component {
   render() {
     return (
       <Screen>
-        <div className="flex w-full justify-center bg-black">
-          <Lottie animationData={heroAnimation} />
+        <Hero
+          title={'Page not found'}
+          subtitle="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+          image={heroAnimation}
+        />
+        <div className="w-full bg-black text-white">
+          <div className="container flex flex-wrap flex-col px-5 mx-auto md:flex-row">
+            Lorem ipsum
+            <div className="flex flex-row w-full mt-0 justify-center text-center md:text-left md:w-2/5"></div>
+          </div>
         </div>
         <Wave />
+        <Welcome />
       </Screen>
     );
   }
