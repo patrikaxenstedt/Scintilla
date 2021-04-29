b83ae6d40b68fc371f54090a58e0233634b70a77
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-29 10:16:11 +0200

    cleaned up code in nav

--- frontend/src/components/Navbar/Nav.js
+++ frontend/src/components/Navbar/Nav.js
@@ -3,19 +3,10 @@ import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logoutUser } from '../../actions/authActions';
-/* import NavItem from './NavItem'; */
 import Image from '../UI/Image';
 import './Nav.css';
 
 class Nav extends Component {
-  /*   state = {
-    isOpen: false,
-  };
-
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  }; */
-
   onLogout(e) {
     e.preventDefault();
     this.props.logoutUser();
@@ -25,49 +16,6 @@ class Nav extends Component {
     const { isAuthenticated, user } = this.props.auth;
 
     return (
-      // <nav className="navbar">
-      //   <div className="nav-center">
-      //     <div className="nav-header">
-      //       <Link to="/content">
-      //         {/* <img src={logo} alt="Logotype" /> */}
-      //         Logo
-      //       </Link>
-      //       <button
-      //         type="button"
-      //         className="nav-btn"
-      //         onClick={this.handleToggle}
-      //       ></button>
-      //     </div>
-      //     {isAuthenticated ? (
-      //       <ul
-      //         className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
-      //       >
-      //         <li>
-      //           <Link to="/content">Welcome {user.name}!</Link>
-      //         </li>
-      //         <li>
-      //           <Link to="/content">Home</Link>
-      //         </li>
-      //         <li>
-      //           <Link to="/content">???</Link>
-      //         </li>
-      //         <li>
-      //           <Link to="/logout" onClick={this.onLogout.bind(this)}>
-      //             Sign out
-      //           </Link>
-      //         </li>
-      //       </ul>
-      //     ) : (
-      //       <ul
-      //         className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
-      //       >
-      //         <li>
-      //           <Link to="/content">Home</Link>
-      //         </li>
-      //       </ul>
-      //     )}
-      //   </div>
-      // </nav>
       <nav className="nav flex flex-wrap items-center justify-between px-4 bg-black text-white">
         <Link
           className="flex flex-no-shrink items-center mr-6 py-3 text-grey-darkest"
