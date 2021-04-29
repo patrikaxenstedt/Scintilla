6b2897145d90520037e4fc8053f9b9e73c4d58d3
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-04-29 13:13:25 +0200

    added user to hero ui and display user on content page

--- frontend/src/components/UI/Hero.js
+++ frontend/src/components/UI/Hero.js
@@ -1,11 +1,14 @@
 import Lottie from 'lottie-react';
 
-const Hero = ({ title, subtitle, image }) => {
+const Hero = ({ title, subtitle, image, user }) => {
   return (
     <div className="w-full bg-black text-white mt-8">
       <div className="container flex flex-wrap flex-col px-5 mx-auto md:flex-row">
         <div className="flex flex-col w-full mt-5 justify-center text-center md:text-left md:w-2/5">
-          <h1 className="my-4 text-5xl font-bold leading-tight">{title}</h1>
+          <h1 className="my-4 text-5xl font-bold leading-tight">
+            {title}
+            {user}!
+          </h1>
           <p className="leading-normal text-xl mb-8">{subtitle}</p>
         </div>
         <div className="flex w-full justify-center md:w-3/5">
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     return (
       <Screen>
         <Hero
-          title={'Welcome to Scintilla!'}
+          title={'Welcome to Scintilla'}
           subtitle="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. "
           image={heroAnimation}
         />
