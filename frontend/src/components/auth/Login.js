ca504d8a1e8d511da53d3b5cc22a051849340909
Patrik Axenstedt <patrikaxenstedt@MacBookPro.local>
2021-05-03 15:18:08 +0200

    added new UI component, styled forms

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
 import classnames from 'classnames';
-
-import Test from '../UI/Test';
+import LoginLottie from '../UI/LoginL';
 import heroAnimation from '../../assets/lottie/signin.json';
 
 class Login extends Component {
@@ -44,86 +43,84 @@ class Login extends Component {
     return (
       <>
         <main>
-          <section className="absolute w-full h-full">
-            <div className="absolute top-0 w-full h-full bg-black">
-              <Test image={heroAnimation} />
-              <div className="container mx-auto px-4">
-                <div className="flex content-center items-center justify-center">
-                  <div className="w-full lg:w-4/12 px-4">
-                    <h1 className="text-5xl font-bold leading-tight text-center mt-14 text-white">
-                      Login here
-                    </h1>
-                    <div className="relative flex flex-col min-w-0 break-words w-full mb-6 ">
-                      <div className="rounded-t mb-0 px-6 py-6">
-                        <div className="text-center mb-3">
-                          <form
-                            onSubmit={this.onSubmitHandler}
-                            className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                          >
-                            <div class="mb-4">
-                              <label class="block text-gray-700 text-sm font-bold mb-2">
-                                Email
-                              </label>
-                              <input
-                                type="text"
-                                name="email"
-                                placeholder="Your email"
-                                className={classnames(
-                                  'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
-                                  {
-                                    'shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline':
-                                      errors.email,
-                                  }
-                                )}
-                                value={email}
-                                onChange={this.onChange}
-                              />
-                              {errors && (
-                                <div className="error-email">
-                                  {errors.email}
-                                </div>
+          <section className="absolute w-full h-full bg-black pt-40">
+            <h1 className="text-5xl font-bold leading-tight text-center mt-14 text-white">
+              Sign in here
+            </h1>
+            <div className="absolute w-full bg-black top-0">
+              <LoginLottie image={heroAnimation} />
+            </div>
+            <div className="container mx-auto px-4">
+              <div className="flex justify-center">
+                <div className="w-full lg:w-4/12 px-4">
+                  <div className="relative flex flex-col min-w-0 break-words w-full mb-6">
+                    <div className="rounded-t mb-0 px-6 py-6">
+                      <div className="text-center mb-3">
+                        <form
+                          onSubmit={this.onSubmitHandler}
+                          className="shadow-md rounded px-8 pt-6 pb-8 mb-4  bg-black bg-opacity-50"
+                        >
+                          <div class="mb-4">
+                            <label class="block text-gray-700 text-sm font-bold mb-2">
+                              Email
+                            </label>
+                            <input
+                              type="text"
+                              name="email"
+                              placeholder="Your email"
+                              className={classnames(
+                                'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                                {
+                                  'shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline':
+                                    errors.email,
+                                }
                               )}
-                            </div>
-                            <div class="mb-6">
-                              <label class="block text-gray-700 text-sm font-bold mb-2">
-                                Password
-                              </label>
-                              <input
-                                type="password"
-                                name="password"
-                                placeholder="Your password"
-                                className={classnames(
-                                  'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
-                                  {
-                                    'shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline':
-                                      errors.password,
-                                  }
-                                )}
-                                value={password}
-                                onChange={this.onChange}
-                              />
-                              {errors && (
-                                <div className="error-password">
-                                  {errors.password}
-                                </div>
+                              value={email}
+                              onChange={this.onChange}
+                            />
+                            {errors && (
+                              <div className="error-email">{errors.email}</div>
+                            )}
+                          </div>
+                          <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2">
+                              Password
+                            </label>
+                            <input
+                              type="password"
+                              name="password"
+                              placeholder="Your password"
+                              className={classnames(
+                                'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                                {
+                                  'shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline':
+                                    errors.password,
+                                }
                               )}
-                            </div>
+                              value={password}
+                              onChange={this.onChange}
+                            />
+                            {errors && (
+                              <div className="error-password">
+                                {errors.password}
+                              </div>
+                            )}
+                          </div>
 
-                            <button
-                              disabled={
-                                !(this.state.email && this.state.password)
-                              }
-                              className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-8 rounded-full m-1.5 "
-                            >
-                              Sign In
+                          <button
+                            disabled={
+                              !(this.state.email && this.state.password)
+                            }
+                            className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-8 rounded-full m-1.5 "
+                          >
+                            Sign In
+                          </button>
+                          <Link to="/">
+                            <button className="bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded-full m-1.5 ">
+                              Back
                             </button>
-                            <Link to="/">
-                              <button className="bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-6 rounded-full m-1.5 ">
-                                Back
-                              </button>
-                            </Link>
-                          </form>
-                        </div>
+                          </Link>
+                        </form>
                       </div>
                     </div>
                   </div>
